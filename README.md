f6497c6ee59f45c498adc82d0b4488bf33d41327
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-25 23:46:10 +0200

    chore: skeleton, tests pending

---
+++ README.md
@@ -0,0 +1,18 @@
+# Golangci-Lint Linter
+
+Linter for the configuration files for the well known linter [`golangci-lint`][golangci-lint].
+
+## ⬇️ Getting Started
+
+TODO
+
+## 🚀 How To Run It
+
+The project is using [uv][uv], to run it:
+
+```bash
+uv run golangci-lint-linter
+```
+
+[golangci-lint]: https://golangci-lint.run/
+[uv]: https://docs.astral.sh/uv/
--- src/golangci_lint_linter/__init__.py
+++ src/golangci_lint_linter/__init__.py
@@ -1,2 +1,23 @@
-def main() -> None:
+from typing import TextIO
+from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedMap
+
+from golangci_lint_linter.errors import ProgramError
+
+
+def _read_yaml_file(f: TextIO) -> None:
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    parsed = yaml.load(f)
+    if not isinstance(parsed, CommentedMap):
+        raise ProgramError(f"Error parsing the yaml file")
+
+
+def main() -> int:
     print("Hello from golangci-lint-linter!")
+    f = open("resources/examples/simple/.golangci.yml")
+    if not f.readable():
+        raise ValueError(f"Cannot read file {f}")
+    _read_yaml_file(f)
+
+    return 0
