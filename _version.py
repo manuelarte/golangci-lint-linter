12dd1088b4f0a025b74e3d1e9d5d20bc521825d4
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-25 23:13:10 +0200

    chore: new format

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g9be9307.d20250625'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g9be9307.d20250625')
+__version__ = version = '0.1.dev2+g620ddcd.d20250625'
+__version_tuple__ = version_tuple = (0, 1, 'dev2', 'g620ddcd.d20250625')
