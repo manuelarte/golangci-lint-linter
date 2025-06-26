d2c433f7dbc155f3b23973ed4c23205209ed54f2
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-06-26 22:15:47 +0200

    chore: adding tests for not valid golangci.yml file

--- tests/rules/test_alphabetical_linters.py
+++ tests/rules/test_alphabetical_linters.py
@@ -59,3 +59,17 @@ linters:
     reports: [Report] = rule.lint(commented_map)
     assert reports is not None
     assert len(reports) == 1
+
+
+def test_not_valid_golangci_but_valid_yaml():
+    f = io.StringIO(
+        """
+{ "not_valid": "not valid golangci.yml file" }
+    """.lstrip()
+    )
+
+    commented_map: CommentedMap = read_yaml_file(f)
+    rule: AlphabeticalLinters = AlphabeticalLinters()
+    reports: [Report] = rule.lint(commented_map)
+    assert reports is not None
+    assert len(reports) == 0
--- tests/rules/test_alphabetical_settings.py
+++ tests/rules/test_alphabetical_settings.py
@@ -50,3 +50,17 @@ linters:
     reports: [Report] = rule.lint(commented_map)
     assert reports is not None
     assert not reports
+
+
+def test_not_valid_golangci_but_valid_yaml():
+    f = io.StringIO(
+        """
+{ "not_valid": "not valid golangci.yml file" }
+    """.lstrip()
+    )
+
+    commented_map: CommentedMap = read_yaml_file(f)
+    rule: AlphabeticalSettings = AlphabeticalSettings()
+    reports: [Report] = rule.lint(commented_map)
+    assert reports is not None
+    assert len(reports) == 0
