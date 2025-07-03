ee342c0f3e59640a608799b27e479e1e1569c070
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-07-03 07:35:41 +0200

    test linters alphabetical fix

--- internal/linters/linters_alphabetical_test.go
+++ internal/linters/linters_alphabetical_test.go
@@ -75,3 +75,104 @@ linters:
 		})
 	}
 }
+
+/*
+func TestLintersAlphabetical_Fix(t *testing.T) {
+	testCases := map[string]struct {
+		input    []byte
+		expected []byte
+	}{
+		"linters field not present": {
+			input: []byte(`
+version: 2
+`),
+			expected: []byte(`
+version: 2
+`),
+		},
+		"linters enable not sorted alphabetically": {
+			input: []byte(`
+version: 2
+linters:
+  enable:
+    - tagliatelle  # make sure all the json tags are following the same pattern
+    - funcorder
+`),
+			expected: []byte(`
+version: 2
+linters:
+  enable:
+	- funcorder
+    - tagliatelle  # make sure all the json tags are following the same pattern
+`),
+		},
+		"linters enable sorted alphabetically": {
+			input: []byte(`
+version: 2
+linters:
+  enable:
+    - funcorder  # method order
+    - tagliatelle
+`),
+			expected: []byte(`
+version: 2
+linters:
+  enable:
+    - funcorder  # method order
+    - tagliatelle
+`),
+		},
+		"linters disable not sorted alphabetically": {
+			input: []byte(`
+version: 2
+linters:
+  disable:
+    - tagliatelle
+    - funcorder
+`),
+			expected: []byte(`
+version: 2
+linters:
+  disable:
+    - funcorder
+    - tagliatelle
+`),
+		},
+		"linters disable sorted alphabetically": {
+			input: []byte(`
+version: 2
+linters:
+  disable:
+    - funcorder
+    - tagliatelle
+`),
+			expected: []byte(`
+version: 2
+linters:
+  disable:
+    - funcorder
+    - tagliatelle
+`),
+		},
+	}
+
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			golangci, err := internal.Parse(test.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			lint := NewLintersAlphabetical()
+			lint.Fix(golangci)
+			actual, err := golangci.(internal.YamlGolangci).Marshal()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(actual, test.expected) {
+				t.Errorf("got %#v, want %#v", string(actual), string(test.expected))
+			}
+		})
+	}
+}
+*/
