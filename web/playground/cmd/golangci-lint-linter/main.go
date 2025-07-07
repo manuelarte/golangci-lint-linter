e048c9ba52611444d2d756f7e785467f1a9989ff
manuelarte <manueldoncelmartos@gmail.com>
2025-07-07 15:48:27 +0200

    wasm

--- web/playground/cmd/golangci-lint-linter/main.go
+++ web/playground/cmd/golangci-lint-linter/main.go
@@ -1,7 +1,26 @@
+//go:build js && wasm
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
+
+func apply(this js.Value, args []js.Value) any {
+	input := args[0].String()
+	msg := fmt.Sprintf("Input: %s\n", input)
+	fmt.Print(msg)
+	return msg
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	// channel is used so we can block on it, otherwise go process will exit when this module is loaded,
+	// and we want the exported functions still available for later executions
+	c := make(chan struct{}, 0)
+
+	js.Global().Set("apply", js.FuncOf(apply))
+
+	fmt.Println("Hello WebAssembly!")
+	<-c
 }
