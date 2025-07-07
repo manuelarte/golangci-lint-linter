31533e9b73164223098fa881478cbc9a9a8740de
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-07-07 14:13:41 +0200

    chore: playground first commit

---
+++ web/playground/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="fill-height" max-width="1400">
+    <v-row>
+      <v-col cols="6">
+        <v-textarea
+          clearable
+          label="Original"
+          row-height="30"
+          rows="30"
+          variant="outlined"
+        />
+      </v-col>
+
+      <v-col cols="6">
+        <v-textarea label="Fixed" row-height="30" rows="30" variant="outlined" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
