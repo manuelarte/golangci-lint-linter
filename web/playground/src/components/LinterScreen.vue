c96b326b77183d38fcef332272aad507c32182df
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-07-08 14:59:47 +0200

    chore: adding v-code-diff

--- web/playground/src/components/LinterScreen.vue
+++ web/playground/src/components/LinterScreen.vue
@@ -1,13 +1,24 @@
 <template>
+  <v-banner
+    class="my-4"
+    color="warning"
+    icon="$warning"
+    lines="two"
+    :sticky="true"
+  >
+    <v-banner-text>
+      This linter is experimental and you should check all the fixed proposed.
+    </v-banner-text>
+  </v-banner>
   <v-container class="fill-height" max-width="1400">
     <v-row>
       <v-col cols="6">
         <v-textarea
           v-model="originalContent"
           clearable
-          label="Original"
-          row-height="30"
-          rows="30"
+          label="Paste here your .golangci.yml"
+          row-height="5"
+          rows="5"
           variant="outlined"
           @click:clear="lint(null)"
           @input="lint($event.target.value)"
@@ -15,13 +26,18 @@
       </v-col>
 
       <v-col cols="6">
-        <v-textarea
-          v-model="proposedContent"
-          label="Fixed"
-          readonly
-          row-height="30"
-          rows="30"
-          variant="outlined"
+        <p>TODO: List of errors</p>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="40">
+        <CodeDiff
+          filename=".golangci.yml"
+          lang="yaml"
+          :new-string="proposedContent"
+          :old-string="originalContent"
+          output-format="side-by-side"
+          theme="dark"
         />
       </v-col>
     </v-row>
@@ -29,6 +45,7 @@
 </template>
 
 <script setup lang="ts">
+  import { CodeDiff } from 'v-code-diff'
   import { ref } from 'vue'
 
   const originalContent: Ref<string | null> = ref(null)
